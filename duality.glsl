55cc5a9d0dd06966ce5d456b18d4d58a015d75f2
frantastic7 <slijepcevicfran@gmail.com>
2023-06-06 22:19:01 +0200

    Duality

---
+++ duality.glsl
@@ -0,0 +1,24 @@
+vec3 specter (float t) {
+    vec3 a = vec3 (0.3,0.0,0.1);
+    vec3 b = vec3 (0.1,0.9,0.0);
+    vec3 c = vec3 (0.0,0.4,0.3);
+    return a*b - sin (t) * (c*(c+t)-b);
+}
+
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 uv = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
+    vec2 uv0 = uv;
+    vec3 finalColor = vec3(0.0);
+
+    for (float i = 0.0; i < 4.5; i++) {
+        uv = fract(uv * 1.5) - 0.5;
+        float d = length(uv) * exp(-length(uv0));
+        vec3 col = specter(length(uv0) + i * 0.3 + iTime * 0.4);
+        d = abs(d)-sin(d * 4.5)/3.0;
+        finalColor += col * d;
+    }
+
+    fragColor = vec4(finalColor, 1.0);
+}
